e69c58e2ea8dd5209dde264cec780109dfcc20c2
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-16 15:30:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function(req, res, next) {
 app.post('/liff', function(req, res, next) {
     var options = {
         method: 'POST',
-        uri: 'https:/lastwebservice.herokuapp.com/test',
+        uri: 'https://gentle-crag-28693.herokuapp.com/test',
         json: true
     };
 
